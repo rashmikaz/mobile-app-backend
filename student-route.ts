5205ca28ca733e39bf126913c6742e8a3ecbf805
rashmikaz <phrashmika@gmail.com>
2025-08-27 09:18:54 +0530

    delete

--- student-route.ts
+++ student-route.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { Student } from "./StudentModel";
-import { studentSave, studentGetAll } from "./student-data-store";
+import {
+  studentSave,
+  studentGetAll,
+  studentDelete,
+} from "./student-data-store";
 
 const router = express.Router();
 
@@ -23,5 +27,16 @@ router.get("/all", async (req, res) => {
     res.status(200).json(students);
   } catch (error) {
     res.status(500).json({ error: "Error fetching students" });
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await studentDelete(Number(id));
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting student" });
   }
 });
