12bc1e9ba5c56db9b34c1a390ce04dcd29480b20
rashmikaz <phrashmika@gmail.com>
2025-09-10 19:36:19 +0530

    route teacher in server

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import studentRoute from "./student-route";
+import teacherRoute from "./teacher-route";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/student", studentRoute);
+app.use("/teacher", teacherRoute);
 
 const PORT = 3000;
 app.listen(PORT, () => {
