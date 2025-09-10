cbc7effc2643ffe352373d07bfe019f456d98af2
rashmikaz <phrashmika@gmail.com>
2025-09-10 22:53:54 +0530

    program done

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import studentRoute from "./route/student-route";
 import teacherRoute from "./route/teacher-route";
+import programRoute from "./route/program-route";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 
 app.use("/student", studentRoute);
 app.use("/teacher", teacherRoute);
+app.use("/program", programRoute);
 
 const PORT = 3000;
 app.listen(PORT, () => {
